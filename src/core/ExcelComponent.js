8b0c72385efb360bd1ee47c807cd826b7a350dc4
Markov Kirill <Ker1n.mkc@gmail.com>
2021-09-09 14:18:08 +0300

    group select

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,14 +3,18 @@ import {DomListener} from '@core/DomListener'
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
-    this.name = options.name || ''
+    this.name = options.name || '';
+    this.prepare()
+  }
+  prepare() { 
+    
   }
 
   // Возвращает шаблон компонента
   toHTML() {
     return ''
   }
-
+  
   init() {
     this.initDOMListeners()
   }
--- src/core/dom.js
+++ src/core/dom.js
@@ -1,82 +1,100 @@
 class Dom {
   constructor(selector) {
-    this.$el = typeof selector === 'string'
-      ? document.querySelector(selector)
-      : selector
+    this.$el =
+      typeof selector === "string"
+        ? document.querySelector(selector)
+        : selector;
   }
 
   html(html) {
-    if (typeof html === 'string') {
-      this.$el.innerHTML = html
-      return this
+    if (typeof html === "string") {
+      this.$el.innerHTML = html;
+      return this;
     }
-    return this.$el.outerHTML.trim()
+    return this.$el.outerHTML.trim();
   }
 
   clear() {
-    this.html('')
-    return this
+    this.html("");
+    return this;
   }
 
   on(eventType, callback) {
-    this.$el.addEventListener(eventType, callback)
+    this.$el.addEventListener(eventType, callback);
   }
 
   off(eventType, callback) {
-    this.$el.removeEventListener(eventType, callback)
+    this.$el.removeEventListener(eventType, callback);
+  }
+
+  find(selector) {
+    return $(this.$el.querySelector(selector));
   }
 
   append(node) {
     if (node instanceof Dom) {
-      node = node.$el
+      node = node.$el;
     }
 
     if (Element.prototype.append) {
-      this.$el.append(node)
+      this.$el.append(node);
     } else {
-      this.$el.appendChild(node)
+      this.$el.appendChild(node);
     }
 
-    return this
+    return this;
   }
 
-  closest(selector) { 
+  get data() {
+    return this.$el.dataset;
+  }
+
+  closest(selector) {
     return $(this.$el.closest(selector));
-  }
-
-
-  findAll(selector) { 
-    return this.$el.querySelectorAll(selector)
   }
 
   getCoordinates() {
     return this.$el.getBoundingClientRect();
   }
 
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector);
+  }
 
   css(styles = {}) {
-    for (const key in styles) {
-      if (styles.hasOwnProperty.call(styles, key)) {
-       this.$el.style[key] = styles[key];
-      }
-    }
-    this.$el
+    Object.keys(styles).forEach((key) => {
+      this.$el.style[key] = styles[key];
+    });
   }
 
-  get data() { 
-    return this.$el.dataset
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(":");
+      return {
+        row: +parsed[0],
+        col: +parsed[1],
+      };
+    }
+    return this.data.id;
+  }
+
+  addClass(className) {
+    this.$el.classList.add(className);
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className);
   }
 }
-
 // event.target
 export function $(selector) {
-  return new Dom(selector)
+  return new Dom(selector);
 }
 
-$.create = (tagName, classes = '') => {
-  const el = document.createElement(tagName)
+$.create = (tagName, classes = "") => {
+  const el = document.createElement(tagName);
   if (classes) {
-    el.classList.add(classes)
+    el.classList.add(classes);
   }
-  return $(el)
-}
+  return $(el);
+};
